a54e151c10deb7eb58767506bb661bb704d8a555
dhanalakshmbharathi <87256496+dhanalakshmbharathi@users.noreply.github.com>
2022-04-18 23:40:39 +0530

    Add files via upload

--- quiz_game/main.py
+++ quiz_game/main.py
@@ -3,11 +3,19 @@ from data import question_data
 from quiz_brain import Quiz_Brain
 question_bank=[]
 for dict in question_data:
-    new_quest=Question(dict["text"],answer=dict["answer"])
+    new_quest=Question(dict["question"],answer=dict["correct_answer"])
     question_bank.append(new_quest)
+def game():
 
-quiz=Quiz_Brain(question_bank)
-while quiz.still_has_questions():
-    quiz.next_question()
-print("Congrats!!You've completed the quiz")
-print(f"Your final score : {quiz.score}/{len(question_bank)}")
+    quiz=Quiz_Brain(question_bank)
+    num=0
+
+    while quiz.still_has_questions() and num<10:
+        quiz.next_question()
+        num+=1
+    print("Congrats!!You've completed the quiz")
+    print(f"Your final score : {quiz.score}/{len(question_bank)}")
+game()
+key=input("Want to start again?(yes/no)")
+if key=="yes":
+    game()
